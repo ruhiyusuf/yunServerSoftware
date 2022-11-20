e0e1a3e309792bf203e35c769c8bc9318bdf6c09
sunnythecode <45048308+sunnythecode@users.noreply.github.com>
2022-11-19 20:07:49 -0800

    jstick_code

--- legacyPython/main.py
+++ legacyPython/main.py
@@ -1,12 +1,14 @@
 import pygame
 from pygame.locals import *
 from UDP import sendUDP
+import time
 
 pygame.init()
 
 joystick = pygame.joystick.Joystick(0)
 
 while True:
+    time.sleep(.005)
     for event in pygame.event.get(): # get the events (update the joystick)
         if event.type == QUIT: # allow to click on the X button to close the window
             pygame.quit()
@@ -16,7 +18,8 @@ while True:
         print("stopped")
         break
     else:
-        x = joystick.get_axis(0)
-        y = joystick.get_axis(1)
+        x = round(joystick.get_axis(2), 3)
+        y = round(-joystick.get_axis(1), 3)
         msg = str(x) + ":" + str(y)
-        sendUDP(msg, IP = "192.168.86.33", port = 2367)
+        print(msg)
+        sendUDP(msg, IP = "192.168.86.33", port = 8080)
